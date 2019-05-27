43a93104ebc5363a0dbde3bf61231f058ef2c3ac
MingliCheng <cml321@hotmail.com>
2019-05-27 19:26:46 +0800

    图书列表删除openid检验

--- controllers/book/bookdetail.js
+++ controllers/book/bookdetail.js
@@ -6,37 +6,25 @@ module.exports = async (ctx) => {
     let isCollect
     if (openid){
         detail = await mysql('books')
-            .select('books.*', 'cSessionInfo.user_info')
-            .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
+            .select('books.*')
             .where('id', id)
             .first()
         isCollect = await mysql('collects').where('collects.open_id', openid).where('collects.item_id',id).where('collects.type_id', 1).count('collects.collect_id as cout')
-        const info = JSON.parse(detail.user_info)
         ctx.state.data = Object.assign({}, detail, {
             tags: detail.tags.split(','),
             summary: detail.summary.split('\n'),
             catalog: detail.catalog.split('\n'),
-            user_info: {
-                name: info.nickName,
-                image: info.avatarUrl
-            },
             isCollect: isCollect[0].cout,
         })
     }else{
         detail = await mysql('books')
-            .select('books.*', 'cSessionInfo.user_info')
-            .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
+            .select('books.*')
             .where('id', id)
             .first()
-        const info = JSON.parse(detail.user_info)
         ctx.state.data = Object.assign({}, detail, {
             tags: detail.tags.split(','),
             summary: detail.summary.split('\n'),
             catalog: detail.catalog.split('\n'),
-            user_info: {
-                name: info.nickName,
-                image: info.avatarUrl
-            },
             isCollect: 0
         })
     }
@@ -46,7 +34,7 @@ module.exports = async (ctx) => {
           .increment('count', 1)
           if(openid) {
               // 首先判断 user_book是否有此信息
-              const userBookId = await mysql('user_book').select('user_book.id').where('user_book.openid', openid).where('user_book.bookid', id).first()              
+              const userBookId = await mysql('user_book').select('user_book.id').where('user_book.openid', openid).where('user_book.bookid', id).first()
               if (userBookId){// 存在
                   await mysql('user_book').where('user_book.id', userBookId.id).increment('visit_number', 1)
               }else{// 不存在 新建
--- controllers/book/booklist.js
+++ controllers/book/booklist.js
@@ -2,33 +2,24 @@
 const { mysql } = require('../../qcloud')
 
 module.exports = async (ctx) => {
-    const { page, openid, size = 5, type } = ctx.request.query
-    if(openid == 'undefined') {
+    let { page, openid, size = 5, type } = ctx.request.query
+    if (openid == 'undefined') {
         openid = false
     }
-    console.log('xxx', ctx.request.query)
-    // const size = 8
     const mysqlSelect = mysql('books')
-        .select('books.*', 'cSessionInfo.user_info')
-        .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
+        .select('books.*')
         .orderBy('books.id', 'desc')
     const total = await mysql('books').count('id as total')
-    console.log('total', total, 'openid', openid )
+    console.log('total', total, 'openid', openid)
     let books
-    // != 'undefined'
-    if (openid) {
-        // 根据opid过滤
-        books = await mysqlSelect.where('books.openid', openid).limit(size).offset(Number(page) * Number(size))
+
+    if (type) {
+        books = await mysqlSelect.limit(size).offset(Number(page) * size).where('books.category',
+            type)
     } else {
-        if (type) {
-            books = await mysqlSelect.limit(size).offset(Number(page) * size).where('books.category',
-                type)
-        } else {
-            // 全部图书 分页
-            books = await mysqlSelect.limit(size).offset(Number(page) * size)
-        }
+        // 全部图书 分页
+        books = await mysqlSelect.limit(size).offset(Number(page) * Number(size))
     }
-    // .orderBy('id','desc')
 
     // 查询是否有推荐信息
     let recommends = []
@@ -58,14 +49,7 @@ module.exports = async (ctx) => {
 
 
     ctx.state.data = {
-        list: books.map(v => {
-            const info = JSON.parse(v.user_info)
-            return Object.assign({}, v, {
-                user_info: {
-                    nickName: info.nickName
-                }
-            })
-        }),
+        list: books,
         recommends,
         advs,
         total: total[0].total
